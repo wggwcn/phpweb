5e0d65d07021488f4e897bf64746092071c7c023
wggwcn <282872666@qq.com>
2017-04-11 17:17:26 +0800

    Update 控制器的继承.php

--- 控制器的继承.php
+++ 控制器的继承.php
@@ -9,5 +9,6 @@ class HelloController extends  Controller{
         echo 'hello world';
     }
 }
+\\在function里面必须使用前置action  结构R 是反馈 HELLO 是控制器 index是hello控制器的执行文件地址
 \\创建的hello控制器反馈，域名用r=hello
 http://localhost/basic/web/index.php?r=hello/index
