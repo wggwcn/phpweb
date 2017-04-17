8ae1754cca597b2b7fb50c2d84fb8b74519fed76
wggwcn <282872666@qq.com>
2017-04-18 03:32:10 +0800

    Update 数据模型之关联查询性能问题.php

--- yii/data/数据模型之关联查询性能问题.php
+++ yii/data/数据模型之关联查询性能问题.php
@@ -1,1 +1,11 @@
-
+<?php
+namespace app\controllers;
+use yii\web\Controller;
+use app\models\Test;
+use app\models\Customer;
+use app\mpdels\Order;
+class HelloController extends Controller{
+  public function actionIndex(){
+    
+  }
+}
