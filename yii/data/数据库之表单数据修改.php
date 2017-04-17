5ab5e0947f750de563f4ff74446ca92083f42921
wggwcn <282872666@qq.com>
2017-04-18 01:41:35 +0800

    Update 数据库之表单数据修改.php

--- yii/data/数据库之表单数据修改.php
+++ yii/data/数据库之表单数据修改.php
@@ -1,1 +1,19 @@
-
+<?php 
+//这里的test 数据已经继承了models avtiverecord的类
+namespace app\controllers;
+use yii\web\Controller;
+//test.php 已经在models创建
+use app\models\Test;
+ class HelloController extends Controller{
+ public function actionIndex(){
+ //修改数据的方法
+ //首先把数据库的字段找出来 使用one快速调用
+ $test = Test::find()->where(['id'=>4])->one();
+ //给test 中需要修改的title 比如修改为title4
+ $test->title = 'title4' ;
+ 然后再保存
+ $test ->save();
+ 
+ }
+ }
+ 
