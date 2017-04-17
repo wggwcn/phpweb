818bc884ee926ae9e32e127f5412bd9166c4f043
wggwcn <282872666@qq.com>
2017-04-18 00:50:46 +0800

    Update 数据库模型之表单删除.php

--- yii/data/数据库模型之表单删除.php
+++ yii/data/数据库模型之表单删除.php
@@ -1,1 +1,18 @@
-
+<?php 
+//这里的test 数据已经继承了models avtiverecord的类
+namespace app\controllers;
+use yii\web\Controller;
+//test.php 已经在models创建
+use app\models\Test;
+ class HelloController extends Controller{
+ public function actionIndex(){
+ //删除数据
+ Test::find()->where(['id'=>1])->all();
+ $results[0]->delete();
+ 
+ // 快速删除 id大于0的值会被删除掉
+ Test::deleteAll('id>:id',array(':id'=>0));
+ 
+ }
+ }
+ 
